10013affb44dd4072e8574b7083f07c622b1c170
wanghaogithub720 <wanghaomspace@gmail.com>
2014-10-23 15:21:38 +0800

    dev IpadGridViewCell

--- dependencies/ipad/YoutubeCollectionView/Pod/Classes/cell/IpadGridViewCell.h
+++ dependencies/ipad/YoutubeCollectionView/Pod/Classes/cell/IpadGridViewCell.h
@@ -23,12 +23,10 @@
 
 @property(strong, nonatomic) IBOutlet UIImageView * thumbnails;
 
-//@property(strong, nonatomic) IBOutlet UILabel * title;
-//
-//@property(strong, nonatomic) IBOutlet UIImageView * userHeader;
-//@property(strong, nonatomic) IBOutlet UILabel * userName;
 
 @property(strong, nonatomic) IBOutlet UIView * infoView;
+
+@property (unsafe_unretained, nonatomic) IBOutlet UIView *userView;
 
 @property(nonatomic, assign) id<IpadGridViewCellDelegate> delegate;
 @property(nonatomic, strong) GTLYouTubeVideo * video;
--- dependencies/ipad/YoutubeCollectionView/Pod/Classes/cell/IpadGridViewCell.m
+++ dependencies/ipad/YoutubeCollectionView/Pod/Classes/cell/IpadGridViewCell.m
@@ -16,34 +16,46 @@
 #import "GTLYouTubeThumbnail.h"
 
 #import "UIImageView+Cache.h"
+#import "IpadGridViewInfoCell.h"
 
 
 @implementation IpadGridViewCell
 
-- (id)initWithFrame:(CGRect)frame {
-   self = [super initWithFrame:frame];
-   if (self) {
-      // 初始化时加载collectionCell.xib文件
-      NSArray * arrayOfViews = [[NSBundle mainBundle] loadNibNamed:@"IpadGridViewCell" owner:self options:nil];
+//- (id)initWithFrame:(CGRect)frame {
+//   self = [super initWithFrame:frame];
+//   if (self) {
+//      // 初始化时加载collectionCell.xib文件
+//      NSArray * arrayOfViews = [[NSBundle mainBundle] loadNibNamed:@"IpadGridViewCell" owner:self options:nil];
+//
+//      // 如果路径不存在，return nil
+//      if (arrayOfViews.count < 1) {
+//         return nil;
+//      }
+//      // 如果xib中view不属于UICollectionViewCell类，return nil
+//      if (![[arrayOfViews objectAtIndex:0] isKindOfClass:[UICollectionViewCell class]]) {
+//         return nil;
+//      }
+//      // 加载nib
+//      self = [arrayOfViews objectAtIndex:0];
+//
+//   }
+//   return self;
+//}
 
-      // 如果路径不存在，return nil
-      if (arrayOfViews.count < 1) {
-         return nil;
-      }
-      // 如果xib中view不属于UICollectionViewCell类，return nil
-      if (![[arrayOfViews objectAtIndex:0] isKindOfClass:[UICollectionViewCell class]]) {
-         return nil;
-      }
-      // 加载nib
-      self = [arrayOfViews objectAtIndex:0];
-   }
-   return self;
+- (void)setupUI {
+   // Add other views
+   NSArray * views = [[NSBundle mainBundle] loadNibNamed:@"IpadGridViewInfoCell" owner:self options:nil]; //&1
+   IpadGridViewInfoCell * infoCell = [views lastObject];
+   infoCell.frame = CGRectMake(0, 0, self.infoView.frame.size.width, self.infoView.frame.size.height);
+   [self.infoView addSubview:infoCell];
 }
 
 
 - (void)bind:(GTLYouTubeVideo *)video placeholderImage:(UIImage *)image delegate:(id<IpadGridViewCellDelegate>)delegate {
    self.video = video;
    self.delegate = delegate;
+
+   [self setupUI];
 
    // Confirm that the result represents a video. Otherwise, the
    // item will not contain a video ID.
