32e7dc2d8812c6bca2ffa29acc279d7d10a85418
wanghaogithub720 <wanghaomspace@gmail.com>
2014-10-23 11:59:06 +0800

    add YoutubeGridViewController.

---
+++ TubeIPadApp/TubeIPadApp/CollectionView/YoutubeGridViewController.h
@@ -0,0 +1,13 @@
+//
+//  YoutubeGridViewController.h
+//  TubeIPadApp
+//
+//  Created by djzhang on 10/23/14.
+//  Copyright (c) 2014 djzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YoutubeGridViewController : UICollectionViewController
+
+@end
