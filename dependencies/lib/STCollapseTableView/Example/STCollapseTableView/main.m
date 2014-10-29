f1bc103ce1fd223595e6bafc7b0b893999ed0ad8
wanghaogithub720 <wanghaomspace@gmail.com>
2014-10-29 12:44:17 +0800

    add library STCollapseTableView.

---
+++ dependencies/lib/STCollapseTableView/Example/STCollapseTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  STCollapseTableView
+//
+//  Created by wanghaogithub720 on 10/29/2014.
+//  Copyright (c) 2014 wanghaogithub720. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ dependencies/lib/STCollapseTableView/Example/Tests/Tests.m
@@ -0,0 +1,9 @@
+//
+//  STCollapseTableViewTests.m
+//  STCollapseTableViewTests
+//
+//  Created by wanghaogithub720 on 10/29/2014.
+//  Copyright (c) 2014 wanghaogithub720. All rights reserved.
+//
+
+${TEST_EXAMPLE}
