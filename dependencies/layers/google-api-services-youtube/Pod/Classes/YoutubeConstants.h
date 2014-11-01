d365f0208330e1da167398276e520c41e6919fdc
wanghaogithub720 <wanghaomspace@gmail.com>
2014-11-01 08:53:54 +0800

    dev youtube oauth.

--- dependencies/layers/google-api-services-youtube/Pod/Classes/YoutubeConstants.h
+++ dependencies/layers/google-api-services-youtube/Pod/Classes/YoutubeConstants.h
@@ -1,12 +1,9 @@
-static NSString * kMyYoutubeApiKey = @"AIzaSyBd9kf5LB41bYWnxI3pfoxHJ2njRvmAA90";
+static NSString * apiKey = @"AIzaSyBd9kf5LB41bYWnxI3pfoxHJ2njRvmAA90";
 static NSString * kMyClientID = @"632947002586-hsu569tme6lt8635vvoofi5mnkqfkqus.apps.googleusercontent.com";
 static NSString * kMyClientSecret = @"dHWxjaetid5ckoVMzp0LmzJt";
+static NSString * scope = @"https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtubepartner https://www.googleapis.com/auth/youtubepartner-channel-audit https://www.googleapis.com/auth/youtube.upload";
 
-//NSString *  = @"981239920851-gi3pis2s62pb2miojr2lsukuc8n8405h.apps.googleusercontent.com";     // pre-assigned by service
-//NSString *  = @"wMEhiReTb1_429VsuG9Xu9r0"; // pre-assigned by service
-
-static NSString * kMyYoutube_scope = @"https://www.googleapis.com/auth/plus.me"; // scope for Google+ API
-static NSString * kKeychainItemName = @"OAuth2: mxyoutube";
+static NSString * kKeychainItemName = @"mxyoutube";
 
 static NSUInteger search_maxResults = 15;
 
