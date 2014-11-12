49a09b04a4ebb921d27a2087b439e0f3c548dd5e
wanghaogithub720 <wanghaomspace@gmail.com>
2014-11-12 16:34:50 +0800

    dev channel page info.

---
+++ dependencies/ipad/YoutubeCollectionView/Pod/Classes/channelpage/YoutubeChannelTopCell.h
@@ -0,0 +1,13 @@
+//
+//  YoutubeChannelTopCell.h
+//  IOSTemplate
+//
+//  Created by djzhang on 11/12/14.
+//  Copyright (c) 2014 djzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YoutubeChannelTopCell : UIView
+
+@end
---
+++ dependencies/ipad/YoutubeCollectionView/Pod/Classes/channelpage/YoutubeChannelTopCell.m
@@ -0,0 +1,21 @@
+//
+//  YoutubeChannelTopCell.m
+//  IOSTemplate
+//
+//  Created by djzhang on 11/12/14.
+//  Copyright (c) 2014 djzhang. All rights reserved.
+//
+
+#import "YoutubeChannelTopCell.h"
+
+@implementation YoutubeChannelTopCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
