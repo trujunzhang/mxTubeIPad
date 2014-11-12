04943c5a23d362fdbaf8185cdccef1b57de8609d
wanghaogithub720 <wanghaomspace@gmail.com>
2014-11-12 15:46:18 +0800

    dev channel page info.

---
+++ dependencies/ipad/YoutubeCollectionView/Pod/Classes/channelpage/YoutubeChannelPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  YoutubeChannelPageViewController.h
+//  IOSTemplate
+//
+//  Created by djzhang on 11/12/14.
+//  Copyright (c) 2014 djzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YoutubeChannelPageViewController : UIViewController
+
+@end
