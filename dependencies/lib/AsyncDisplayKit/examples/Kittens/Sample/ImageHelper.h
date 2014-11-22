ffc7f21add7bd31af85097fc3ae8491368b79d5b
wanghaogithub720 <wanghaomspace@gmail.com>
2014-11-22 11:35:44 +0800

    dev search suggestion by AFNetworking.

---
+++ dependencies/lib/AsyncDisplayKit/examples/Kittens/Sample/ImageHelper.h
@@ -0,0 +1,11 @@
+//
+// Created by djzhang on 11/22/14.
+// Copyright (c) 2014 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ImageHelper : NSObject
++ (NSArray *)getImageArray;
+@end
