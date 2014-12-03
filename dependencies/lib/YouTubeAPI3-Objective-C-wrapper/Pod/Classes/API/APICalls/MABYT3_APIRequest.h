a9a2af6d9a4b103a323b871ff9a45dd9f7681d43
wanghaogithub720 <wanghaomspace@gmail.com>
2014-12-03 15:38:03 +0800

    dev subscription list by afnetworking.[ok]

--- dependencies/lib/YouTubeAPI3-Objective-C-wrapper/Pod/Classes/API/APICalls/MABYT3_APIRequest.h
+++ dependencies/lib/YouTubeAPI3-Objective-C-wrapper/Pod/Classes/API/APICalls/MABYT3_APIRequest.h
@@ -29,7 +29,6 @@ static NSString * kKeychainItemName = @"mxyoutube";
 static NSUInteger search_maxResults = 20;
 
 
-
 @interface MABYT3_AutoCompleteRequest : AFHTTPSessionManager
 @property(nonatomic, strong) NSURLSessionDataTask * lastTask;
 + (MABYT3_AutoCompleteRequest *)sharedInstance;
@@ -59,12 +58,6 @@ static NSUInteger search_maxResults = 20;
 - (NSURLSessionDataTask *)LISTPlayListForURL:(NSMutableDictionary *)parameters completion:(MABYoutubeResponseBlock)completion;
 - (NSURLSessionDataTask *)LISTActivitiesForURL:(NSMutableDictionary *)parameters completion:(MABYoutubeResponseBlock)completion;
 - (NSURLSessionDataTask *)searchForParameters:(NSMutableDictionary *)parameters completion:(MABYoutubeResponseBlock)completion;
-- (void)fetchWithUrl:(NSString *)urlStr andHandler:(MABYoutubeResponseBlock)handler;
-- (void)LISTPlayListItemsForURL:(NSString *)urlStr andHandler:(MABYoutubeResponseBlock)handler;
-- (void)LISTPlayListsForURL:(NSString *)urlStr andHandler:(MABYoutubeResponseBlock)handler;
-- (void)LISTVideoCategoriesForURL:(NSString *)urlStr andHandler:(void (^)(NSMutableArray *, NSError *))handler;
-- (void)LISTSearchItemsForURL:(NSString *)urlStr andHandler:(MABYoutubeResponseBlock)handler;
-- (void)LISTSubscriptionsForURL:(NSString *)urlStr andHandler:(MABYoutubeResponseBlock)handler;
 
 
 - (void)LIKEVideo:(NSString *)videoId andHandler:(void (^)(NSError *, BOOL))handler;
